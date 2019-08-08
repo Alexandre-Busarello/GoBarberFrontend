e38436dd2ac838a0d9f50ecd514a66473835fc10
Alexandre Busarello <busamar@LAPTOP-NKAJQ9SE.localdomain>
2019-08-08 17:02:20 +0000

    Starting code versioning

---
+++ src/styles/global.js
@@ -0,0 +1,54 @@
+import { createGlobalStyle, keyframes } from 'styled-components';
+
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    background: linear-gradient(-90deg, #7159c1, #ab59c1);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
